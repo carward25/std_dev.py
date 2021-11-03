3741e9efccd3df9a8c1113343ad84b4d8ef781c2
carward25 <83989145+carward25@users.noreply.github.com>
2021-11-03 15:30:23 -0700

    Update std_dev.py

--- std_dev.py
+++ std_dev.py
@@ -3,18 +3,46 @@
 #description: this program takes as a parameter a
 #list of Person objects and returns the standard deviation of all their ages (
 
+
 class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+def __init__(self, name, age):
+self.name = name
+self.age = age
 
 
-def std_dev(person_list):
-    m = 0
-    for person in person_list:
-        m += person.age
-    m /= len(person_list)
-    total = 0
-    for person in person_list:
-        total += (person.age - m) ** 2
-    return (total / (len(person_list) - 1)) ** 0.5
+def showPerson(personList):
+for person in personList:
+print("Name: ", person.name, "\t Age: ", person.age)
+
+
+def standardDeviation(personList, length):
+total, mean, sd = 0.0, 0.0, 0.0
+
+
+for person in personList:
+
+total += person.age
+
+
+mean = total / length
+
+
+for person in personList:
+# Calculates standard deviation
+sd += math.pow(person.age - mean, 2);
+# Returns standard deviation
+return math.sqrt(sd / length);
+
+
+# Creaates an array of objects of class Person
+personList = []
+personList.append(Person("Pyari",40))
+personList.append(Person("Mohan", 22))
+personList.append(Person("Sasmita", 35))
+personList.append(Person("Manvi", 12))
+personList.append(Person("Tanvi", 10))
+
+showPerson(personList)
+
+# displays return value
+print("\n Standard Deviation = ", standardDeviation(personList, 5))
