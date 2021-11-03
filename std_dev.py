693fd556c0b180b5faaecc2c4a3607bb33f263e8
carward25 <83989145+carward25@users.noreply.github.com>
2021-11-03 15:33:08 -0700

    Update std_dev.py

--- std_dev.py
+++ std_dev.py
@@ -3,26 +3,32 @@
 #description: this program takes as a parameter a
 #list of Person objects and returns the standard deviation of all their ages (
 
-
 class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+def __init__(self,name,age):
+self.name = name
+self.age = age
+  
+def std_dev(persons):
+ages = []
+for p in persons:
+ages.append(p.age)
+  
+total = sum(ages)
+mean = total/len(ages)
+  
+total = 0
+for a in ages:
+total += (a-mean)**2;
+  
+total /=len(ages)
+  
+dev = total**.5
+return dev
 
-
-def std_dev(person_list):
-    m = 0
-    for person in person_list:
-        m += person.age
-    m /= len(person_list)
-    total = 0
-    for person in person_list:
-        total += (person.age - m) ** 2
-    return (total / (len(person_list) - 1)) ** 0.5
-
-
+if __name__ == '__main__':
 p1 = Person("Kyoungmin", 73)
 p2 = Person("Mercedes", 24)
 p3 = Person("Beatrice", 48)
 person_list = [p1, p2, p3]
-print(std_dev(person_list))
+answer = std_dev(person_list)
+print("Standard deviation is : ",answer)
